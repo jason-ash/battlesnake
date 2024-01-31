6d2cb8d73cf75d425654e87b97e4708d51b239d0
Jason Ash <ash.jasont@gmail.com>
2024-01-31 12:28:33 -0800

    update router definition

--- src/web/router/mod.rs
+++ src/web/router/mod.rs
@@ -1,12 +1,5 @@
-use crate::State;
-use axum::{response::IntoResponse, routing::get, Router};
-
 mod index;
 
-pub fn router(state: State) -> Router {
-    Router::new().merge(index::router()).with_state(state)
-}
-
-async fn handler() -> impl IntoResponse {
-    "hello world!"
+pub fn router(state: crate::State) -> axum::Router {
+    axum::Router::new().merge(index::router()).with_state(state)
 }
