fb1f6725941ad29b2cface3f440303c994d395de
Jason Ash <ash.jasont@gmail.com>
2024-01-31 15:57:03 -0800

    adding more api json objects

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,5 @@
+mod battlesnake;
+mod board;
 mod game;
 mod payload;
 
--- src/model/payload/move_.rs
+++ src/model/payload/move_.rs
@@ -1,5 +1,24 @@
+use crate::model::{battlesnake::Battlesnake, board::Board, game::Game};
+
 #[derive(Debug, serde::Deserialize)]
-pub struct MoveRequest {}
+pub struct MoveRequestPayload {
+    pub game: Game,
+    pub turn: u32,
+    pub board: Board,
+    pub you: Battlesnake,
+}
 
 #[derive(Debug, serde::Serialize)]
-pub struct MoveResponse {}
+pub struct MoveResponsePayload {
+    #[serde(rename = "move")]
+    pub move_: Move,
+    pub shout: String,
+}
+
+#[derive(Debug, serde::Serialize)]
+pub enum Move {
+    Up,
+    Down,
+    Left,
+    Right,
+}
