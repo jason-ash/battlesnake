791d9ba215f841806ec2860eb7fd6690c18d2c51
Jason Ash <ash.jasont@gmail.com>
2024-01-31 20:23:46 -0800

    add battlesnake struct fields

--- src/model/battlesnake.rs
+++ src/model/battlesnake.rs
@@ -1,2 +1,28 @@
+use std::collections::VecDeque;
+
 #[derive(Debug, serde::Deserialize)]
-pub struct Battlesnake {}
+pub struct Battlesnake {
+    pub id: String,
+    pub name: String,
+    pub health: u8,
+    pub body: VecDeque<Position>,
+    pub latency: u32,
+    pub head: Position,
+    pub length: u8,
+    pub shout: String,
+    pub squad: String,
+    pub customizations: Customizations,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Position {
+    pub x: u8,
+    pub y: u8,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Customizations {
+    pub color: String,
+    pub head: String,
+    pub tail: String,
+}
