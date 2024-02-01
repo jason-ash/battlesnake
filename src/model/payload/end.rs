a111ca69fa27f0988eae9e5dae199c5e64127f0e
Jason Ash <ash.jasont@gmail.com>
2024-01-31 20:46:17 -0800

    complete the fields in the EndGamePayload

--- src/model/payload/end.rs
+++ src/model/payload/end.rs
@@ -1,2 +1,9 @@
+use super::{Battlesnake, Board, Game};
+
 #[derive(Debug, serde::Deserialize)]
-pub struct EndGamePayload {}
+pub struct EndGamePayload {
+    pub game: Game,
+    pub turn: u32,
+    pub board: Board,
+    pub you: Battlesnake,
+}
