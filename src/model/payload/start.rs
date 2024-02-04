8ea613ebb82dc96fdebcd787aa56d74371902a3e
Jason Ash <ash.jasont@gmail.com>
2024-02-03 20:23:15 -0800

    add fields to the StartPayload

--- src/model/payload/start.rs
+++ src/model/payload/start.rs
@@ -1,2 +1,9 @@
+use super::{Battlesnake, Board, Game};
+
 #[derive(Debug, serde::Deserialize)]
-pub struct StartGamePayload {}
+pub struct StartGamePayload {
+    game: Game,
+    turn: u32,
+    board: Board,
+    you: Battlesnake,
+}
